4347331cb59ceb49fb922b9412b4a3b59c6c578d
NdKai <NdKai@MSI>
2024-02-06 15:13:33 +0700

    init project

---
+++ android/app/src/main/kotlin/com/example/zens_evaluation_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zens_evaluation_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/helper.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: avoid_print
+
+import 'dart:async';
+import 'dart:convert' as convert;
+import 'dart:io' as io;
+
+import 'package:flutter/services.dart';
+
+
+class Helper {
+
+
+}
